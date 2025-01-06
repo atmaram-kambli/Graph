6e22365b41e972369dce9f7537fb829dc964afa8
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2025-01-06 15:17:42 +0530

    Disjoint Set (Union By Size)

--- disjoint_set.cpp
+++ disjoint_set.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 class DisjointSet {
   private: 
-    vector<int>rank, parent;
+    vector<int>rank, parent, siz;
   public:
     DisjointSet(int n) {
         rank.resize(n+1, 0);
+        siz.resize(n+1, 1);
         parent.resize(n+1, 0);
         for(int i = 0; i <= n; i++) {
             parent[i] = i;
@@ -35,16 +36,34 @@ class DisjointSet {
             rank[ulp_v]++;              // increase the rank of parent by 1
         }
     }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = parent[u];          // ulp_u --> ultimate parent of u
+        int ulp_v = parent[v];          // ulp_v --> ultimate parent of v
+
+        if(ulp_u == ulp_v) return;          // if parent of u and v are same, no need to perform unionBySize
+
+        if(siz[ulp_u] < siz[ulp_v]) {       // if ulp_u has smaller siz than that of ulp_v
+            parent[ulp_u] = ulp_v;          // make ulp_v as parent of ulp_u
+            siz[ulp_v] += siz[ulp_u];       // update the siz of parent
+        }
+        else {                              // // if ulp_u has smaller or equal siz to that of ulp_v
+            parent[ulp_v] = ulp_u;          // make ulp_v as parent of ulp_u
+            siz[ulp_u] += siz[ulp_v];       // update the siz of parent
+        }
+    }
+
+
 };
 
 int main() {
     // Example
     DisjointSet ds(7);
-    ds.unionByRank(1, 2);
-    ds.unionByRank(2, 3);
-    ds.unionByRank(4, 5);
-    ds.unionByRank(6, 7);
-    ds.unionByRank(5, 6);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
 
     if(ds.findParent(3) == ds.findParent(7)) {
         cout << "Same component!\n";
@@ -55,7 +74,7 @@ int main() {
     ds.unionByRank(3, 7);
 
     if(ds.findParent(3) == ds.findParent(7)) {
-        cout << "Same component!\n";
+        cout << "Same component!!\n";
     }else {
         cout << "Not same component!\n";
     }
